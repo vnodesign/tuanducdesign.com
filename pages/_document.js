56147e09dc05ede319b1b8f7b5ccf766f48f04e5
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2023-01-14 09:41:59 +0700

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -29,14 +29,14 @@ export default class Document extends NextDocument {
               `,
             }}
           />
-          <script
-            async
-            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4183134625750063"
-          />
         </Head>
         <body className="vno-bg-white vno-text-slate-500 vno-antialiased dark:vno-bg-slate-900 dark:vno-text-slate-400">
           <Main />
           <NextScript />
+          <script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4183134625750063"
+          />
         </body>
       </Html>
     )
