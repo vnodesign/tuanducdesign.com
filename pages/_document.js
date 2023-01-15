18592ea5361cfcdfd365394398d46cfb4756981f
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2023-01-16 02:27:59 +0700

    Update layout

--- pages/_document.js
+++ pages/_document.js
@@ -12,23 +12,10 @@ export default class Document extends NextDocument {
       <Html lang={siteMetadata.language}>
         <Head>
           <link rel="shortcut icon" href="/static/CA0E67E9-AAD0-4D36-82D8-674C7504DFD1.jpg" />
-          <link rel="manifest" href="/static/manifest.webmanifest" />
-          <meta name="theme-color" content="#f8fafc" />
+          <link rel="manifest" href="/static/site.manifest" />
+          <meta name="theme-color" content="#f8fafc" media="(prefers-color-scheme: light)" />
+          <meta name="theme-color" content="#0B1120" media="(prefers-color-scheme: dark)" />
           <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                try {
-                  if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-                    document.documentElement.classList.add('dark')
-                    document.querySelector('meta[name="theme-color"]').setAttribute('content', '#0B1120')
-                  } else {
-                    document.documentElement.classList.remove('dark')
-                  }
-                } catch (_) {}
-              `,
-            }}
-          />
         </Head>
         <body className="vno-bg-white vno-text-slate-500 vno-antialiased dark:vno-bg-slate-900 dark:vno-text-slate-400">
           <Main />
