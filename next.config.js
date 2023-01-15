cb58cd58041b2d268cad01461577ee3b316d42b2
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2023-01-15 22:22:47 +0700

    Fix build

--- next.config.js
+++ next.config.js
@@ -1,24 +1,33 @@
-const withPWA = require("next-pwa")
-
-module.exports = withPWA({
-  reactStrictMode: true,
-  swcMinify: true,
-  pageExtensions: ['js', 'jsx', 'md', 'mdx'],
-  eslint: {
-    dirs: ['pages', 'components', 'lib', 'layouts'],
-  },
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    })
-
-    return config
-  },
-  pwa:{
-    dest:"public",
-    register:true,
-    skipWaiting:true,
-    disable:process.env.NODE_ENV === 'development'
-  }
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
 })
+
+const withPWA = require('next-pwa')
+
+module.exports = withBundleAnalyzer(
+  withPWA({
+    reactStrictMode: true,
+    swcMinify: true,
+    experimental: {
+      scrollRestoration: true,
+    },
+    pageExtensions: ['js', 'jsx', 'md', 'mdx'],
+    eslint: {
+      dirs: ['pages', 'components', 'lib', 'layouts'],
+    },
+    webpack: (config) => {
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      })
+
+      return config
+    },
+    pwa: {
+      dest: 'public',
+      register: true,
+      skipWaiting: true,
+      disable: process.env.NODE_ENV === 'development',
+    },
+  })
+)
