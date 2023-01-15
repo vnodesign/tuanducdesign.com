bf1f78c280b74d695eab52d9782307708e18d78a
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2023-01-16 00:12:28 +0700

    Fix robots

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: process.env.SITE_URL || 'https://tuanducdesign.com',
-  generateRobotsTxt: true, // (optional)
+  generateRobotsTxt: false, // (optional)
   // ...other options
 }
