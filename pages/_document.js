1d48b6a7d16fb72ac1c3a893d24c982a4270c64a
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2023-01-27 02:01:17 +0700

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -20,14 +20,12 @@ export default class Document extends NextDocument {
           <script
             dangerouslySetInnerHTML={{
               __html: `
-                try {
-                  if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-                    document.documentElement.classList.add('dark')
-                    document.querySelector('meta[name="theme-color"]').setAttribute('content', '#0B1120')
-                  } else {
-                    document.documentElement.classList.remove('dark')
-                  }
-                } catch (_) {}
+                  if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+        document.documentElement.classList.add('dark');
+        document.querySelector('meta[name="theme-color"]').setAttribute('content', '#0B1120')
+    } else {
+        document.documentElement.classList.remove('dark')
+    }
               `,
             }}
           />
