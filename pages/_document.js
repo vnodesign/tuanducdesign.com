7166662a87aaa5d3363b3ff69dd4c66f1f9ae9e3
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2023-01-29 18:27:02 +0700

    Update tailwind config

--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,6 @@
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
 import siteMetadata from '@/data/siteMetadata'
 import { Analytics } from '@vercel/analytics/react'
-import ReactGA from 'react-ga'
 
 export default class Document extends NextDocument {
   static async getInitialProps(ctx) {
@@ -10,24 +9,41 @@ export default class Document extends NextDocument {
   }
 
   render() {
-    ReactGA.initialize(`${siteMetadata.analytics.googleAnalyticsId}`)
     return (
-      <Html lang={siteMetadata.language}>
+      <Html lang={siteMetadata.language} className="dark">
         <Head>
           <link rel="shortcut icon" href="/static/CA0E67E9-AAD0-4D36-82D8-674C7504DFD1.jpg" />
           <link rel="manifest" href="/static/site.manifest" />
           <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
           <link rel="dns-prefetch" href="//www.googletagmanager.com" />
-          <link rel="dns-prefetch" href="//stats.g.doubleclick.net" />
           <link rel="dns-prefetch" href="//pagead2.googlesyndication.com" />
-          <link rel="dns-prefetch" href="//www.googletagservices.com" />
-          <link rel="dns-prefetch" href="//adservice.google.com" />
-          <meta name="theme-color" content="#ffffff" />
+          <meta name="theme-color" content="#f8fafc" />
+          <script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4183134625750063"
+            crossOrigin="anonymous"
+          />
         </Head>
         <body className="vno-bg-white vno-text-slate-500 vno-antialiased dark:vno-bg-slate-900 dark:vno-text-slate-400">
           <Main />
           <NextScript />
           <Analytics />
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${siteMetadata.analytics.googleAnalyticsId}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${siteMetadata.analytics.googleAnalyticsId}', {
+                    page_path: window.location.pathname,
+                  });
+              `,
+            }}
+          />
         </body>
       </Html>
     )
