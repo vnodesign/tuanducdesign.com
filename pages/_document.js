986b5d5aa76a01887feed0cb9e2ba87c79969a92
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2023-01-30 18:13:49 +0700

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,30 @@ export default class Document extends NextDocument {
   render() {
     return (
       <Html lang={siteMetadata.language} className="dark">
-        <Head />
+        <Head>
+        <link rel="shortcut icon" href={siteMetadata.siteLogo} />
+        <link rel="manifest" href="/static/site.manifest" />
+        <link rel="dns-prefetch" href="//www.googletagmanager.com" />
+        <link rel="dns-prefetch" href="//stats.g.doubleclick.net" />
+        <link rel="dns-prefetch" href="//pagead2.googlesyndication.com" />
+        <link rel="dns-prefetch" href="//www.googletagservices.com" />
+        <link rel="dns-prefetch" href="//adservice.google.com" />
+        <meta name="theme-color" content="#ffffff" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                try {
+                  if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+                    document.documentElement.classList.add('dark')
+                    document.querySelector('meta[name="theme-color"]').setAttribute('content', '#0B1120')
+                  } else {
+                    document.documentElement.classList.remove('dark')
+                  }
+                } catch (_) {}
+              `,
+          }}
+        />
+      </Head>
         <body className="vno-bg-white vno-text-slate-500 vno-antialiased dark:vno-bg-slate-900 dark:vno-text-slate-400">
           <Main />
           <NextScript />
