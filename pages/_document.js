970ccc844d9f9d93952aa42bd48ade3e32e07ea9
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2023-02-01 00:53:15 +0700

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,27 +1,6 @@
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
 import siteMetadata from '@/data/siteMetadata'
 
-import * as fs from 'fs'
-import * as path from 'path'
-
-class InlineStylesHead extends Head {
-  getCssLinks(files) {
-    return files.sharedFiles
-      .filter((file) => /\.css$/.test(file))
-      .filter((file) => fs.existsSync(path.join(process.cwd(), '.next', file)))
-      .map((file) => (
-        <style
-          key={file}
-          nonce={this.props.nonce}
-          data-href={`${this.context.assetPrefix}/_next/${file}`}
-          dangerouslySetInnerHTML={{
-            __html: fs.readFileSync(path.join(process.cwd(), '.next', file), 'utf-8'),
-          }}
-        />
-      ))
-  }
-}
-
 export default class Document extends NextDocument {
   static async getInitialProps(ctx) {
     const initialProps = await NextDocument.getInitialProps(ctx)
@@ -31,7 +10,7 @@ export default class Document extends NextDocument {
   render() {
     return (
       <Html lang={siteMetadata.language} className="dark">
-        <InlineStylesHead>
+        <Head>
           <link rel="shortcut icon" href={siteMetadata.siteLogo} />
           <link rel="manifest" href="/static/site.manifest" />
           <link rel="dns-prefetch" href="//www.googletagmanager.com" />
@@ -59,7 +38,7 @@ export default class Document extends NextDocument {
             src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4183134625750063"
             crossOrigin="anonymous"
           ></script>
-        </InlineStylesHead>
+        </Head>
         <body className="vno-bg-white vno-text-slate-500 vno-antialiased dark:vno-bg-slate-900 dark:vno-text-slate-400">
           <Main />
           <NextScript />
