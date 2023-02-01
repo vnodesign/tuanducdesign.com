cc6a213061a10be10e772f4d416d1b691570b0a3
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2023-02-01 12:22:45 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,38 +6,37 @@ const withPWA = require('next-pwa')({
   dest: 'public',
 })
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  pageExtensions: ['js', 'jsx', 'md', 'mdx'],
-  experimental: {
-    scrollRestoration: true,
-  },
-  eslint: {
-    dirs: ['pages', 'components', 'lib', 'layouts'],
-  },
-  async redirects() {
-    return require('./redirects.json')
-  },
-  webpack: (config, { dev, isServer }) => {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    })
-    // Note, preact is only enabled for production builds (`next build`)
-    if (!dev && !isServer) {
-      config.resolve.alias = {
-        ...config.resolve.alias,
-        'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
-        react: 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat',
+module.exports = withBundleAnalyzer(
+  withPWA({
+    reactStrictMode: true,
+    swcMinify: true,
+    pageExtensions: ['js', 'jsx', 'md', 'mdx'],
+    experimental: {
+      scrollRestoration: true,
+    },
+    eslint: {
+      dirs: ['pages', 'components', 'lib', 'layouts'],
+    },
+    async redirects() {
+      return require('./redirects.json')
+    },
+    webpack: (config, { dev, isServer }) => {
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      })
+      // Note, preact is only enabled for production builds (`next build`)
+      if (!dev && !isServer) {
+        config.resolve.alias = {
+          ...config.resolve.alias,
+          'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
+          react: 'preact/compat',
+          'react-dom/test-utils': 'preact/test-utils',
+          'react-dom': 'preact/compat',
+        }
       }
-    }
 
-    return config
-  },
-}
-
-module.exports = withBundleAnalyzer(withPWA(nextConfig))
+      return config
+    },
+  })
+)
