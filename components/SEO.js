342cbcd86b5da3c61e4e392fa69f0613d547376b
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2023-02-02 18:51:21 +0700

    Update SEO.js

--- components/SEO.js
+++ components/SEO.js
@@ -134,7 +134,6 @@ const CommonSEO = ({ title, description, openGraph, twImage }) => {
               try {
                 if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
                   document.documentElement.classList.add('dark')
-                  document.querySelector('meta[name="theme-color"]').setAttribute('content', '#0B1120')
                 } else {
                   document.documentElement.classList.remove('dark')
                 }
