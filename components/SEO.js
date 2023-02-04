83cc1f37f713afc54d69e33d1debc17c234d2a60
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2023-02-04 22:16:47 +0700

    Update next config

--- components/SEO.js
+++ components/SEO.js
@@ -26,107 +26,31 @@ const CommonSEO = ({ title, description, openGraph, twImage }) => {
           maxImagePreview: 'none',
           maxVideoPreview: -1,
         }}
-        additionalLinkTags={[
-          {
-            rel: 'manifest',
-            href: '/static/manifest.json',
-          },
-          {
-            rel: 'apple-touch-icon',
-            sizes: '57x57',
-            href: '/static/apple-touch-icon-57x57.png',
-          },
-          {
-            rel: 'apple-touch-icon',
-            sizes: '72x72',
-            href: '/static/apple-touch-icon-72x72.png',
-          },
-          {
-            rel: 'apple-touch-icon',
-            sizes: '76x76',
-            href: '/static/apple-touch-icon-76x76.png',
-          },
-          {
-            rel: 'apple-touch-icon',
-            sizes: '114x114',
-            href: '/static/apple-touch-icon-114x114.png',
-          },
-          {
-            rel: 'apple-touch-icon',
-            sizes: '120x120',
-            href: '/static/apple-touch-icon-120x120.png',
-          },
-          {
-            rel: 'apple-touch-icon',
-            sizes: '144x144',
-            href: '/static/apple-touch-icon-144x144.png',
-          },
-          {
-            rel: 'apple-touch-icon',
-            sizes: '152x152',
-            href: '/static/apple-touch-icon-152x152.png',
-          },
-          {
-            rel: 'apple-touch-icon',
-            sizes: '180x180',
-            href: '/static/apple-touch-icon-180x180.png',
-          },
-          {
-            rel: 'shortcut icon',
-            href: siteMetadata.siteLogo,
-          },
-          {
-            rel: 'dns-prefetch',
-            href: '//www.googletagmanager.com',
-          },
-          {
-            rel: 'dns-prefetch',
-            href: '//stats.g.doubleclick.net',
-          },
-          {
-            rel: 'dns-prefetch',
-            href: '//pagead2.googlesyndication.com',
-          },
-          {
-            rel: 'dns-prefetch',
-            href: '//www.googletagservices.com',
-          },
-          {
-            rel: 'dns-prefetch',
-            href: '//adservice.google.com',
-          },
-        ]}
-        additionalMetaTags={[
-          {
-            content: '100005485267478',
-            property: 'fb:admins',
-          },
-          {
-            content: title,
-            name: 'twitter:title',
-          },
-          {
-            content: description,
-            name: 'twitter:description',
-          },
-          {
-            content: twImage,
-            name: 'twitter:image',
-          },
-          {
-            content: siteMetadata.title,
-            name: 'application-name',
-          },
-          {
-            content: siteMetadata.title,
-            name: 'apple-mobile-web-app-title',
-          },
-          {
-            content: '#38bdf8',
-            name: 'msapplication-TitleColor',
-          },
-        ]}
       />
+      <Head>
+        <link rel="dns-prefetch" href="//www.googletagmanager.com" />
+        <link rel="dns-prefetch" href="//stats.g.doubleclick.net" />
+        <link rel="dns-prefetch" href="//pagead2.googlesyndication.com" />
+        <link rel="dns-prefetch" href="//www.googletagservices.com" />
+        <link rel="dns-prefetch" href="//adservice.google.com" />
+        <link rel="shortcut icon" href={siteMetadata.siteLogo} />
+        <link rel="manifest" href="/static/manifest.json" />
+        <link rel="apple-touch-icon" sizes="57x57" href="/static/apple-touch-icon-57x57.png" />
+        <link rel="apple-touch-icon" sizes="72x72" href="/static/apple-touch-icon-72x72.png" />
+        <link rel="apple-touch-icon" sizes="57x57" href="/static/apple-touch-icon-57x57.png" />
+        <link rel="apple-touch-icon" sizes="76x76" href="/static/apple-touch-icon-76x76.png" />
+        <link rel="apple-touch-icon" sizes="114x114" href="/static/apple-touch-icon-114x114.png" />
+        <link rel="apple-touch-icon" sizes="120x120" href="/static/apple-touch-icon-120x120.png" />
+        <link rel="apple-touch-icon" sizes="144x144" href="/static/apple-touch-icon-144x144.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/static/apple-touch-icon-152x152.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/static/apple-touch-icon-180x180.png" />
+        <meta name="application-name" content={siteMetadata.title} />
+        <meta name="apple-mobile-web-app-title" content={siteMetadata.title} />
+        <meta name="fb:admins" content="100005485267478" />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:image" content={twImage} />
+      </Head>
     </>
   )
 }
