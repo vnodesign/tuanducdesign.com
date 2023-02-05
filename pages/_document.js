52a74e0d1aa0911cdf7e2f722c9cdc8c75d5d5d3
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2023-02-05 18:29:03 +0700

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -11,21 +11,9 @@ export default class Document extends NextDocument {
     return (
       <Html
         lang={siteMetadata.language}
-        className="dark [--scroll-mt:9.875rem] lg:[--scroll-mt:6.3125rem]"
+        className="[--scroll-mt:9.875rem] lg:[--scroll-mt:6.3125rem]"
       >
-        <Head>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-                  document.documentElement.classList.add('dark')
-                } else {
-                  document.documentElement.classList.remove('dark')
-                }
-            `,
-            }}
-          />
-        </Head>
+        <Head />
         <body className="vno-bg-white vno-text-slate-500 vno-antialiased dark:vno-bg-slate-900 dark:vno-text-slate-400">
           <Main />
           <NextScript />
