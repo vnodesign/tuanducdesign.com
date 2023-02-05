2f3b725828fc75c6149c2ca33cc30c6c393b6d95
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2023-02-05 15:37:40 +0700

    Fix function

--- next.config.js
+++ next.config.js
@@ -143,20 +143,11 @@ const nextConfig = {
       },
     ]
   },
-  webpack: (config, { dev, isServer }) => {
+  webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/,
       use: ['@svgr/webpack'],
     })
-
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
-        react: 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat',
-      })
-    }
 
     return config
   },
