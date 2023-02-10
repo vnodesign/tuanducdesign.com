54c696b5fe66da2c0ea4af7d2c68d7e2dc104fce
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2023-02-10 18:36:38 +0700

    Update LatestPosts.js

--- components/home/LatestPosts.js
+++ components/home/LatestPosts.js
@@ -4,12 +4,12 @@ import Image from '@/components/Image'
 
 export default function LatestPosts({ posts, totalPages }) {
   return (
-    <section className="vno-relative vno-my-10 vno-py-10 md:vno-my-20 vno-px-8">
+    <section className="vno-relative vno-my-10 vno-py-10 md:vno-my-20">
       <div>
         <div className="vno-absolute vno-inset-x-0 vno-top-0 vno-hidden vno-h-[37.5rem] vno-bg-gradient-to-b vno-from-[#0c1120] dark:vno-block"></div>
         <div className="vno-absolute vno-inset-x-0 vno-top-0 vno-h-[37.5rem] vno-bg-top vno-bg-grid-slate-900/[0.04] [mask-image:linear-gradient(0deg,transparent,black)] dark:vno-bg-[center_top_-1px] dark:vno-bg-grid-slate-100/[0.03]"></div>
       </div>
-      <div className="vno-relative vno-mx-auto vno-max-w-7xl">
+      <div className="vno-relative vno-mx-auto vno-max-w-8xl vno-px-4 md:vno-px-5">
         <h2 className="vno-text-4xl vno-font-extrabold vno-tracking-tight vno-text-slate-900 dark:vno-text-white sm:vno-text-5xl">
           <span className="vno-bg-gradient-to-r vno-from-sky-400 vno-to-sky-500 vno-bg-clip-text vno-text-transparent">
             Bài viết mới đăng
